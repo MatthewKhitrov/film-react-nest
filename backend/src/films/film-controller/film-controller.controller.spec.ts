bbfb8bad349465e99eab1f4feef17a964460d2ac
Матвей Хитров <Khitrovmatvei@yandex.ru>
2025-06-28 23:29:50 +0600

    1 коммит для докера

---
+++ backend/src/films/film-controller/film-controller.controller.spec.ts
@@ -0,0 +1,57 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { FilmControllerController } from './film-controller.controller';
+import { FilmsService } from '../film-service/film-service';
+import { fixtures } from '../../films.fixtures';
+
+describe('FilmControllerController', () => {
+  let controller: FilmControllerController;
+  let filmsService: FilmsService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [FilmControllerController],
+      providers: [
+        FilmsService,
+        {
+          provide: 'FilmRepository',
+          useValue: {
+            find: jest.fn().mockResolvedValue([fixtures.film]),
+          },
+        },
+        {
+          provide: 'ScheduleRepository',
+          useValue: {
+            find: jest.fn().mockResolvedValue(fixtures.film.schedule),
+          },
+        },
+      ],
+    }).compile();
+
+    controller = module.get<FilmControllerController>(FilmControllerController)
+    filmsService = module.get<FilmsService>(FilmsService);
+  });
+
+  it('test find all', async () => {
+    expect(controller).toBeDefined();
+    const findResult = await controller.findAll();
+    expect(findResult).toEqual({
+      total: 1,
+      items: [fixtures.film],
+    });
+  });
+
+  it('test find one', async () => {
+    expect(controller).toBeDefined();
+    const findResult = await controller.findOne('0e33c7f6-27a7-4aa0-8e61-65d7e5effecf');
+    expect(findResult).toEqual({
+      total: fixtures.film.schedule.length,
+      items: fixtures.film.schedule.map(item => ({
+        ...item,
+        taken: item.taken.split(', '),
+      })),
+    });
+  });
+  
+});
+
+
---
+++ backend/src/logger/tskv.logger.spec.ts
@@ -0,0 +1,24 @@
+import { TskvLogger } from './tskv.logger';
+
+describe.only('TskvLogger', () => {
+  let logger: TskvLogger;
+  let consoleWarnMock: jest.SpyInstance;
+
+  beforeEach(() => {
+    logger = new TskvLogger();
+    consoleWarnMock = jest.spyOn(console, 'warn').mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    consoleWarnMock.mockRestore();
+  });
+
+  it('should log warn message with optional params in correct format', () => {
+    logger.warn('alarm', { a: 'b', c: 1 });
+    
+    expect(consoleWarnMock).toHaveBeenCalledTimes(1);
+    expect(consoleWarnMock).toHaveBeenCalledWith(
+      'level=warn\tmessage=alarm\toptional=[{"a":"b","c":1}]'
+    );
+  });
+});
